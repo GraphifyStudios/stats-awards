2c637be199ec722758245208d7225507b02ccfc5
ToastedToast <hey@toastify.tk>
2023-11-25 20:00:57 +0800

    add redirect to survey (#1)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  redirects: async () => [{
+    source: "/survey",
+    destination: "https://docs.google.com/forms/d/e/1FAIpQLSfWJxjsYPxzLuZJ8jNcyLOAhXUDg7N2JRIQshlagCJHAyIowA/viewform",
+    permanent: false,
+  }]
+}
 
 module.exports = nextConfig
